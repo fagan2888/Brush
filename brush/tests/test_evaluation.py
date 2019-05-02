8de7374089261e4f4277a377c8bd853ed1b8c618
Tilak Raj Singh <tilak72@gmail.com>
2019-05-02 19:38:13 -0400

    listing of all tests to be written

---
+++ brush/tests/test_evaluation.py
@@ -0,0 +1,24 @@
+# tests the Individual class.
+
+
+from brush.evaluation import Evaluation
+from brush.data import Data
+from brush.population import Population
+from brush.parameters import Parameters
+from sklearn.datasets import load_diabetes
+
+'''
+TODO
+	- mse
+	- bal_accuracy
+	- log_loss
+	- multi_log_loss
+	- fitness
+	- out_ml
+'''
+
+
+def test_fitness():
+    """Check evaluation fitness"""
+    
+
--- brush/tests/test_individual.py
+++ brush/tests/test_individual.py
@@ -3,6 +3,14 @@
 
 from brush.population import Individual
 
+'''
+TODO
+	- EvalEquation
+	- Check_Dominance
+	- Subtree
+	- Complexity
+'''
+
 def test_fit():
     """Individual successfully fits to data"""
     Ind = Individual()
